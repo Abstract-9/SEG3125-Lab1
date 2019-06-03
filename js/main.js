14335f0eb6cbf758d8e8e194bccace4b847b12af
Logan Rodie <lrodi045@uottawa.ca>
2019-06-03 12:19:52 -0400

    started adding dynamic content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(document).load({
+$(document).load = function(){
 
-});
+    $('#search-icon').on('click', function(){
+        let content = $('#search').text();
+        // Do some next level search ;)
+        window.location.href = 'resturantList.html'
+    })
+};
+
+
+
+
+
+function setupClicks() {
+}
