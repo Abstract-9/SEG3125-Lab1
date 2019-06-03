bb6b27706a7427a7a682ec3723d26cb067de4f6f
Logan Rodie <lrodi045@uottawa.ca>
2019-06-03 13:03:09 -0400

    Setup Menu Page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ $(document).ready( function() {
         let content = $('#search').text();
         // Do some next level search ;)
         $(location).attr('href', 'restaurantList.html')
-    })
+    });
 });
 
+function getDB() {
+    return JSON.parse(localStorage.getItem('2148a562ad895344c8d2db6f6ac89f54'));
+}
+
+function setDB(data) {
+    localStorage.setItem('2148a562ad895344c8d2db6f6ac89f54', JSON.stringify(data))
+}
